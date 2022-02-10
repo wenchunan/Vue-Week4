27f66be61edf5070402ca090bc30ab586b4a709a
wenchunan <wenchunan18@gmail.com>
2022-02-10 14:47:46 +0800

    pagination修改

--- js/pagination.js
+++ js/pagination.js
@@ -3,19 +3,24 @@ export default {
   template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item" :class="{disabled: !pages.has_pre}">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link" href="#" aria-label="Previous"  @click.prevent="emitPages(pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li class="page-item" v-for="page in pages.total_pages" :key="page + 'page'" 
       :class="{active : page === pages.current_page}">
-         <a class="page-link" href="#" @click="$emit('get-data',page)">{{page}}</a>
+         <a class="page-link" href="#" @click.prevent="emitPages(page)">{{page}}</a>
       </li>
       <li class="page-item" :class="{disabled: !pages.has_next}">
-        <a class="page-link" href="#" aria-label="Next">
+        <a class="page-link" href="#" aria-label="Next"  @click.prevent="emitPages(pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
     </ul>
   </nav>`,
+  methods: {
+    emitPages(item) {
+      this.$emit("emit-pages", item);
+    },
+  },
 };
